322aa0565c821b2505000c010116a799b52aa88a
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-18 15:02:51 +0500

    goes in

--- javascript2/index.js
+++ javascript2/index.js
@@ -428,4 +428,26 @@ console.log("zaibi is a hecker");
 setTimeout(() => {
     console.log("i am inside timeout")
 }, 2000);
+setTimeout(() => {
+  console.log("i am inside timeout 2");
+}, 2000);
 console.log("the end");
+
+const callback=(arg) => {
+console.log(arg);
+}
+
+const loadscript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+     sc.onload = callback("hasnain");
+    document.head.append(sc);
+    }
+    loadscript(
+      "https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback
+    );
+    
+    
+    
+  
+
