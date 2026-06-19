1e166c74048b2fee7691c42a47c1614695c057e9
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-19 17:04:40 +0500

    goes off

--- new.js
+++ new.js
@@ -11,8 +11,21 @@ else{
   }, 3000);
 }
 });
-prom1.then((a) => {
-  console.log(a);
-}).catch((err)=>{
-  console.log(err)
-})
+// console.log("this is promises");
+let prom2 = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("no randon number is supporting you 2");
+  } else {
+    setTimeout(() => {
+      console.log("yes i am done 2");
+      resolve("hasnain 2");
+    }, 1000);
+  }
+});
+let p3 = Promise.any([prom1, prom2])
+p3.then((a)=>{
+    console.log(a)
+}).catch(err=>{
+    console.log(err)
+});
