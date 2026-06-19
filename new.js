4d8af26dc65185d616f898d804cd62e2fae8cd72
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-19 16:30:09 +0500

    learning

--- new.js
+++ new.js
@@ -1,12 +1,18 @@
-console.log('this is promises');
-let prom1 = new promises((resolve,reject)=>{
-    setTimeout(() =>{
-     console.log('yes i am done');
-     resolve("hasnain")
-    }, 3000);
-
-})
-prom1.then(() =>{
-    console.log(a);
-    
+console.log("this is promises");
+let prom1 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a<0.5){
+    reject("no randon number is supporting you")
+}
+else{
+  setTimeout(() => {
+    console.log("yes i am done");
+    resolve("hasnain");
+  }, 3000);
+}
 });
+prom1.then((a) => {
+  console.log(a);
+}).catch((err)=>{
+  console.log(err)
+})
