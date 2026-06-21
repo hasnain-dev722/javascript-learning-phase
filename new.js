4ec2c269c672cfa83c8562d2939d8f3b5c992cd9
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-21 16:06:26 +0500

    api learned

--- new.js
+++ new.js
@@ -38,7 +38,18 @@
 // })
 // }
 async function getdata() {
-  let x = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // let x = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  let x = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
   let data = await x.json()
     console.log(data)
    return 455
