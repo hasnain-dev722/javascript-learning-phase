28779952c4a49ca3914c99e349d0b65b3d258000
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-21 13:09:33 +0500

    apis

--- new.js
+++ new.js
@@ -1,31 +1,49 @@
-console.log("this is promises");
-let prom1 = new Promise((resolve, reject) => {
-    let a = Math.random();
-    if (a<0.5){
-    reject("no randon number is supporting you")
-}
-else{
-  setTimeout(() => {
-    console.log("yes i am done");
-    resolve("hasnain");
-  }, 3000);
-}
-});
 // console.log("this is promises");
-let prom2 = new Promise((resolve, reject) => {
-  let a = Math.random();
-  if (a < 0.5) {
-    reject("no randon number is supporting you 2");
-  } else {
-    setTimeout(() => {
-      console.log("yes i am done 2");
-      resolve("hasnain 2");
-    }, 1000);
-  }
-});
-let p3 = Promise.any([prom1, prom2])
-p3.then((a)=>{
-    console.log(a)
-}).catch(err=>{
-    console.log(err)
-});
+// let prom1 = new Promise((resolve, reject) => {
+//     let a = Math.random();
+//     if (a<0.5){
+//     reject("no randon number is supporting you")
+// }
+// else{
+//   setTimeout(() => {
+//     console.log("yes i am done");
+//     resolve("hasnain");
+//   }, 3000);
+// }
+// });
+// // console.log("this is promises");
+// let prom2 = new Promise((resolve, reject) => {
+//   let a = Math.random();
+//   if (a < 0.5) {
+//     reject("no randon number is supporting you 2");
+//   } else {
+//     setTimeout(() => {
+//       console.log("yes i am done 2");
+//       resolve("hasnain 2");
+//     }, 1000);
+//   }
+// });
+// let p3 = Promise.any([prom1, prom2])
+// p3.then((a)=>{
+//     console.log(a)
+// }).catch(err=>{
+//     console.log(err)
+// });
+
+function getdata(){
+return new Promise((resolve, reject)=>{
+ setTimeout(()=>{
+  resolve(455)
+ }, timeout);
+})
+}
+console.log("loading modules")
+
+console.log("do something else")
+
+console.log("load data")
+let data = getdata()
+console.log(data)
+
+
+console.log("process data")
