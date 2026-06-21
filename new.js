b01f335db4c15ea9674f20b436261e56997e8e0a
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-21 14:37:06 +0500

    promises

--- new.js
+++ new.js
@@ -30,19 +30,35 @@
 //     console.log(err)
 // });
 
-function getdata(){
+ async function getdata(){
 return new Promise((resolve, reject)=>{
  setTimeout(()=>{
-  resolve(455)
+  reject(455)
  }, 3500);
 })
 }
+async function main() {
+  
 console.log("loading modules")
 
 console.log("do something else")
 
 console.log("load data")
-let data = getdata()
+let data = await getdata()
 console.log(data)
-
 console.log("process data")
+
+ console.log("hasnain")
+ console.log("abbas")
+}
+main()
+
+// data.then((v)=>{
+
+
+
+// console.log("process data")
+
+// console.log("hasnain")
+// console.log("abbas")
+// })
