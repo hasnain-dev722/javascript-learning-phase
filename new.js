a13785ab2985cb03cac55121f8a8bc4581df4d92
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-21 14:59:58 +0500

    api learning

--- new.js
+++ new.js
@@ -37,6 +37,12 @@ return new Promise((resolve, reject)=>{
  }, 3500);
 })
 }
+async function getdata() {
+  let x = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  let data = await x.json()
+    console.log(data)
+   return 455
+  }
 async function main() {
   
 console.log("loading modules")
@@ -44,8 +50,13 @@ console.log("loading modules")
 console.log("do something else")
 
 console.log("load data")
+try {
 let data = await getdata()
 console.log(data)
+}
+catch{
+  console.log("program war gya")
+}
 console.log("process data")
 
  console.log("hasnain")
