a6673e6a76d1b005616912d7abf0ade34b31f998
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-30 19:33:02 +0500

    understand how to put varibales in set interval

--- project 3/script.js
+++ project 3/script.js
@@ -12,3 +12,4 @@ myinput.addEventListener("keyup", function (event) {
 });
 // learn keyup
 // learn if you have to do something with input you have to do something with .value
+// learn how to add variables in set setInterval
