5db5db8516e5d0159a6cecee9143ef4b1f4fc18d
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-30 19:01:59 +0500

    project 3 in process

---
+++ project 3/script.js
@@ -0,0 +1,12 @@
+const mybutton = document.querySelector(".btn");
+let count = 0;
+mybutton.addEventListener("click", () => {
+  setInterval(function () {
+    count = count + 1;
+    mybutton.innerHTML = count;
+  }, 1000);
+});
+const myinput = document.querySelector("input");
+myinput.addEventListener("keyup", function (event) {
+  console.log("user nai dabaya:", event.key);
+});
