2b306610ebce5989187bc13e0296b995b0d567ea
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-07-02 16:37:15 +0500

    mine first api project

--- MY FIRST API PROJECT WEATHER APP/script.js
+++ MY FIRST API PROJECT WEATHER APP/script.js
@@ -1,10 +1,13 @@
-let mybutton =document.querySelector(".btn");
+let mybutton = document.querySelector(".btn");
 let mysearch = document.querySelector(".search");
-mybutton.addEventListener("click", function(){
+mybutton.addEventListener("click", () => {
+  console.log("islamabad");
   getweather();
-  mysearch.addEventListener("keyup", function(event)){
-    if(event.key === "enter"){
-        getweather();
-    }
+});
+
+mysearch.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    console.log("islamabad");
+    getweather();
   }
-})
+});
